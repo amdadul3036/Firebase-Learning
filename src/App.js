c45756a1922a7334806bbe8e47eb01de0bb64048
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-09 10:36:19 -0700

    2nd day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
 
-function App() {
+
+firebase.initializeApp(firebaseConfig);
+
+
+function App() 
+{
+  
+  const [user , setUser] = useState({
+    isSignIn: false,
+    name: "",
+    email: "",
+    passowrd: "",
+    photo:""
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      const {displayName , email , photoURL} = result.user;
+
+      const signedInUser = {
+        isSignIn: true,
+        name: displayName,
+        email: email,
+        photo:photoURL
+      }
+
+      setUser(signedInUser);
+      console.log(displayName , email , photoURL);
+      // // This gives you a Google Access Token. You can use it to access the Google API.
+      // var token = result.credential.accessToken;
+      // // The signed-in user info.
+      // var user = result.user;
+      // // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(error);
+      console.log(errorMessage);
+    });
+
+  }
+   
+
+  const handleSignOut = () => {
+    firebase.auth().signOut().then(function() {
+      const signedOutUser = {isSignIn : false,
+      name:'',
+      photo:'',
+      email: ''
+      }
+      setUser(signedOutUser);
+      
+    }).catch(function(error) {
+      console.log();
+    });
+  }
+
+  const handleSubmit = () =>{
+
+  }
+
+  const handleChange = (event) => {
+          console.log(event.target.name , event.target.value);
+          const isFormValid = true ;
+          if(event.target.name === "Email"){
+            isFormValid = /\S+@\S+\.\S+/.test(event.target.value);
+          }
+
+          if(event.target.name === "Password"){
+            const isPasswordValid = event.target.value.length > 6;
+            const passwordHasNumber =  /\d{1}/.test(event.target.value)
+            isFormValid = isPasswordValid && passwordHasNumber;
+          }
+
+          if(isFormValid){
+             const newUserInfo = {...user};
+             newUserInfo [event.target.name] = event.target.value;
+             setUser(newUserInfo);
+          }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ {
+         user.isSignIn? <button onClick = {handleSignOut}>Sign Out</button>:
+         <button onClick = {handleSignIn}>Sign In</button>
+
+ }
+
+
+      {
+        user.isSignIn && 
+        <div>
+        <p>Welcome , {user.name}</p>
+        <h4>Your Email: {user.email}</h4>
+        <img src ={user.photo} alt= "Your Image" style = {{width: '100px'}}></img>
+        </div>
+      }
+
+
+      <h1>Our own Authentication </h1>
+      <p>Email: {user.email}</p>
+      <p>Password: {user.password}</p>
+      <form onSubmit = {handleSubmit}>
+          <input onChange = {handleChange} type="text" name="Email" placeholder = "Your Email" required/>
+          <br/>
+          <input onChange = {handleChange} type="password" name="Password" id="" placeholder = "Password" required/>
+          <br/>
+          <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2fnj7RUGWBpOEBPOI0qwUd1u4AHeX3CM",
+    authDomain: "ema-john-website-created.firebaseapp.com",
+    databaseURL: "https://ema-john-website-created.firebaseio.com",
+    projectId: "ema-john-website-created",
+    storageBucket: "ema-john-website-created.appspot.com",
+    messagingSenderId: "843237520474",
+    appId: "1:843237520474:web:afe7da3743f850a2f35463"
+  };
+
+  export default firebaseConfig;
